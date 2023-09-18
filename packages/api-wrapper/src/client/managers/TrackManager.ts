1106c784d3a3b1921b5b3ef790e1ff173a9c3fee
Davi Patricio <davipatricio@pm.me>
2023-09-18 14:53:23 -0300

    add `TrackManager#search()`

--- packages/api-wrapper/src/client/managers/TrackManager.ts
+++ packages/api-wrapper/src/client/managers/TrackManager.ts
@@ -1,15 +1,25 @@
+import type { PartialTrack as RawPartialTrack, Track as RawTrack } from '@lastfm-ts/api-types';
+import { PartialTrack } from '../../structures';
 import { Track } from '../../structures/Track';
 import type { Client } from '../Client';
 
 export class TrackManager {
   public constructor(public client: Client) {}
 
-  public async get(artist: string, track: string): Promise<Track> {
+  public async get(artist: string, track: string) {
     const res = await this.client.rest.request('GET', 'track.getinfo', {
       artist,
       track,
     });
 
-    return new Track(res.track);
+    return new Track(res.track as RawTrack);
+  }
+
+  public async search(query: string) {
+    const res = await this.client.rest.request('GET', 'track.search', {
+      track: query,
+    });
+
+    return (res.results.trackmatches.track as RawPartialTrack[]).map(track => new PartialTrack(track));
   }
 }
