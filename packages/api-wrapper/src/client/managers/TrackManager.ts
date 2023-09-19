a249d6a788d310d7eba639ba43eb73ce5cf3be94
Davi Patricio <davipatricio@pm.me>
2023-09-18 21:43:44 -0300

    add initial `TrackManager#search`

--- packages/api-wrapper/src/client/managers/TrackManager.ts
+++ packages/api-wrapper/src/client/managers/TrackManager.ts
@@ -3,14 +3,31 @@ import { PartialTrack } from '../../structures';
 import { Track } from '../../structures/Track';
 import type { Client } from '../Client';
 
+interface TrackGetOptions {
+  searchIfNotFound?: boolean;
+}
+
 export class TrackManager {
   public constructor(public client: Client) {}
 
-  public async get(artist: string, track: string) {
+  public async get(artist: string, track: string, options?: TrackGetOptions): Promise<Track>;
+  public async get(
+    artist: string,
+    track: string,
+    options: TrackGetOptions & { searchIfNotFound: true },
+  ): Promise<PartialTrack | Track>;
+  public async get(artist: string, track: string, options: TrackGetOptions = {}) {
     const res = await this.client.rest.request('GET', 'track.getinfo', {
       artist,
       track,
     });
+
+    if (res.error === 6 && options.searchIfNotFound) {
+      const search = await this.search(`${artist} - ${track}`);
+      if (search.length === 0) throw new Error('No results found');
+
+      return search[0];
+    }
 
     return new Track(res.track as RawTrack);
   }
