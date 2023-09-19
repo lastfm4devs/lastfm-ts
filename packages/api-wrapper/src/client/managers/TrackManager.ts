34c8ee6075ead7e35ad7d44fb5c107b467d98f5d
Davi Patricio <davipatricio@pm.me>
2023-09-18 21:59:48 -0300

    add docs & example for `TrackManager#search`

--- packages/api-wrapper/src/client/managers/TrackManager.ts
+++ packages/api-wrapper/src/client/managers/TrackManager.ts
@@ -3,19 +3,43 @@ import { PartialTrack } from '../../structures';
 import { Track } from '../../structures/Track';
 import type { Client } from '../Client';
 
+/**
+ * Options for the track get request
+ */
 interface TrackGetOptions {
+  /**
+   * Whether to search for the track if it is not found
+   */
   searchIfNotFound?: boolean;
 }
 
 export class TrackManager {
   public constructor(public client: Client) {}
 
-  public async get(artist: string, track: string, options?: TrackGetOptions): Promise<Track>;
+  /**
+   * Get a track by its name and artist
+   *
+   * @param artist - The artist name
+   * @param track - The track name
+   * @param options - Additional options for the request
+   * @returns The track object, or a partial track if the track is not found but the searchIfNotFound option is true
+   * @example
+   * ```ts
+   * const track = await client.tracks.get('DROELOE', 'Sunburn');
+   * console.log(`${track.name} by ${track.artist.name}`); // Sunburn by DROELOE
+   * ```
+   * @example
+   * ```ts
+   * const track = await client.tracks.get('DROELOE', 'Sun bur', { searchIfNotFound: true });
+   * console.log(`${track.name} by ${track.artist.name}`); // Sunburn by DROELOE
+   * ```
+   */
   public async get(
     artist: string,
     track: string,
     options: TrackGetOptions & { searchIfNotFound: true },
   ): Promise<PartialTrack | Track>;
+  public async get(artist: string, track: string, options?: TrackGetOptions): Promise<Track>;
   public async get(artist: string, track: string, options: TrackGetOptions = {}) {
     const res = await this.client.rest.request('GET', 'track.getinfo', {
       artist,
