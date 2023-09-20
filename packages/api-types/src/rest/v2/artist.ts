9a645162e597fea614e7e0a629d43a84ea062196
Davi Patricio <davipatricio@pm.me>
2023-09-19 23:17:35 -0300

    types: add `APIGetSimilarTrack`

--- packages/api-types/src/rest/v2/artist.ts
+++ packages/api-types/src/rest/v2/artist.ts
@@ -1,8 +1,32 @@
-import type { APIArtist } from '../../payloads';
+import type { APIArtist, APIImage, ArtistBasicInfo, StringBoolean } from '../../payloads';
 
 /**
  * Represents a response from the `artist.getinfo` method
  */
 export interface APIGetArtistInfo {
   artist: APIArtist;
+}
+
+/**
+ * Represents a response from the `artist.getsimilar` method
+ */
+export interface APIGetSimilarTrack {
+  similarartists: {
+    artist: (ArtistBasicInfo & {
+      /**
+       * The image for the artist
+       */
+      image: APIImage[];
+
+      // In similartracks, match is a number, but here it's a string
+      /**
+       * The match percentage of the track. This is a number between 0 and 1
+       */
+      match: string;
+      /**
+       * Whether the artist is streamable
+       */
+      streamable: StringBoolean;
+    })[];
+  };
 }
