368ef46d9818df9c33132c5bf996f15a8aef0a2c
Davi Patricio <davipatricio@pm.me>
2023-09-22 14:17:39 -0300

    feat: initial unit test

---
+++ packages/api-wrapper/jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+  testMatch: ['<rootDir>/tests/**/*.spec.ts'],
+};
---
+++ packages/api-wrapper/tests/client.spec.ts
@@ -0,0 +1,7 @@
+describe('Client - TrackManager', () => {
+  describe('get()', () => {
+    it('should fetch a track by name and artist', async () => {
+      expect(1 + 1).toBe(2);
+    });
+  });
+});
